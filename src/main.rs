fa13bab5c6b2acdecb92ed84a041ba151f37f59e
y01and3 <91734856+y01and3@users.noreply.github.com>
2023-09-19 14:06:35 +0800

    finish "A Freestanding Rust Binary"

--- src/main.rs
+++ src/main.rs
@@ -1,2 +1,14 @@
 #![no_std]
-fn main() {}
+#![no_main]
+
+use core::panic::PanicInfo;
+
+#[panic_handler]
+fn panic(_info: &PanicInfo) -> ! {
+    loop {}
+}
+
+#[no_mangle]
+pub extern "C" fn _start() -> ! {
+    loop {}
+}
