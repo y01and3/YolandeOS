2663f01153a8b4d1e0d780bdf6e8620d9022b2a3
y01and3 <91734856+y01and3@users.noreply.github.com>
2023-09-20 22:05:31 +0800

    finish "Printing to Screen" part

--- src/main.rs
+++ src/main.rs
@@ -8,7 +8,23 @@ fn panic(_info: &PanicInfo) -> ! {
     loop {}
 }
 
+struct _VgaText{
+    character: u8,
+    attribute: u8,
+}
+
+static HELLO: &[u8] = b"Hello World!";
+
 #[no_mangle]
 pub extern "C" fn _start() -> ! {
+    let vga_buffer = 0xb8000 as *mut u8;
+
+    for (i, &byte) in HELLO.iter().enumerate() {
+        unsafe {
+            *vga_buffer.offset(i as isize * 2) = byte;
+            *vga_buffer.offset(i as isize * 2 + 1) = 0xb;
+        }
+    }
+
     loop {}
 }
